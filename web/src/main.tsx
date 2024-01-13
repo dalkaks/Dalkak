a91bb1b1ba098fab5f7fdb888fb7cf4dfd89dca9
Louis-Gil <lotusnate@gmail.com>
2024-01-14 00:10:48 +0900

    chore : Add metamask provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { MetaMaskUIProvider } from '@metamask/sdk-react-ui';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<MetaMaskUIProvider
+			debug={false}
+			sdkOptions={{
+				dappMetadata: {
+					name: 'Example React Dapp',
+					url: window.location.host,
+				},
+				// Other options
+			}}
+		>
+			<App />
+		</MetaMaskUIProvider>
+	</React.StrictMode>
+);
