5744d93cdf4b10ee365234cfbf38e3d3ca719993
Louis-Gil <lotusnate@gmail.com>
2024-01-19 23:56:06 +0900

    chore : Add shadcn ui theme

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { Inter as FontSans } from 'next/font/google';
+import type { AppProps } from 'next/app';
+
+export const fontSans = FontSans({
+	subsets: ['latin'],
+	variable: '--font-sans',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div style={{ fontFamily: fontSans.variable }}>
+			<Component {...pageProps} />;
+		</div>
+	);
 }
