2685e872f2f37cde7ca720eb3e8f438ec57e35ab
Louis-Gil <lotusnate@gmail.com>
2024-01-21 00:44:28 +0900

    feat : Add web header metamask

--- web/src/components/common/header.tsx
+++ web/src/components/common/header.tsx
@@ -7,28 +7,33 @@ import {
   navigationMenuTriggerStyle,
 } from '../ui/navigation-menu'
 import Link from 'next/link'
+import { MetaMaskButton } from '@metamask/sdk-react-ui'
 
 export default function Header() {
   return (
-    <NavigationMenu className="h-16">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              <Image
-                src="/favicon.ico"
-                alt="Image"
-                width={25}
-                height={25}
-                className="rounded-md object-cover"
-              />
-              <h1 className="ml-2 text-2xl font-extrabold tracking-tight">
-                dalkak
-              </h1>
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="flex justify-between items-center h-16">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                <Image
+                  src="/favicon.ico"
+                  alt="Image"
+                  width={25}
+                  height={25}
+                  className="rounded-md object-cover"
+                />
+                <h1 className="ml-2 text-2xl font-extrabold tracking-tight">
+                  dalkak
+                </h1>
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <MetaMaskButton theme={'light'} color="white"></MetaMaskButton>
+    </div>
   )
 }
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,8 +4,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Layout from '@/components/layout'
+import { MetaMaskUIProvider } from '@metamask/sdk-react-ui'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [dappUrl, setDappUrl] = useState('')
+
+  useEffect(() => {
+    setDappUrl(window.location.href)
+  }, [])
+
   return (
     <>
       <Head>
@@ -15,9 +23,19 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <div>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <MetaMaskUIProvider
+          sdkOptions={{
+            dappMetadata: {
+              name: 'dalkak dapp',
+              url: dappUrl,
+            },
+            // Other options
+          }}
+        >
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </MetaMaskUIProvider>
       </div>
     </>
   )
