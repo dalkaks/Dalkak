c363135f9c0a5d082e832b91cf89a50f97b2cc3f
Louis-Gil <lotusnate@gmail.com>
2024-01-20 18:56:33 +0900

    chore : Add web font

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import '@/styles/globals.css';
-import { Inter as FontSans } from 'next/font/google';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-
-export const fontSans = FontSans({
-	subsets: ['latin'],
-	variable: '--font-sans',
-});
+import Layout from '@/components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
@@ -17,8 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
 					content="width=device-width, initial-scale=1.0, viewport-fit=cover"
 				/>
 			</Head>
-			<div style={{ fontFamily: fontSans.variable }}>
-				<Component {...pageProps} />;
+			<div >
+				<Layout>
+					<Component {...pageProps} />;
+				</Layout>
 			</div>
 		</>
 	);
