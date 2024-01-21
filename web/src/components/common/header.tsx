c26224a7761f7740b145cfd71ac12ddf6a31065e
Louis-Gil <lotusnate@gmail.com>
2024-01-21 11:33:55 +0900

    feat : Add user info comp

--- web/src/components/common/header.tsx
+++ web/src/components/common/header.tsx
@@ -7,22 +7,9 @@ import {
   navigationMenuTriggerStyle,
 } from '../ui/navigation-menu'
 import Link from 'next/link'
-import { useSDK } from '@metamask/sdk-react'
-import { useState } from 'react'
+import UserInfo from './userInfo'
 
 export default function Header() {
-  const [account, setAccount] = useState<string>()
-  const { sdk, connected, connecting, provider, chainId } = useSDK()
-
-  const connect = async () => {
-    try {
-      const accounts: any = await sdk?.connect()
-      setAccount(accounts?.[0])
-    } catch (err) {
-      console.warn(`failed to connect..`, err)
-    }
-  }
-
   return (
     <div className="flex justify-between items-center h-16">
       <NavigationMenu>
@@ -46,22 +33,7 @@ export default function Header() {
         </NavigationMenuList>
       </NavigationMenu>
 
-      <div>
-        {!connected && !connecting && (
-          <button style={{ padding: 10, margin: 10 }} onClick={connect}>
-            Connect
-          </button>
-        )}
-        {connected && (
-          <div>
-            <>
-              {chainId && `Connected chain: ${chainId}`}
-              <p></p>
-              {account && `Connected account: ${account}`}
-            </>
-          </div>
-        )}
-      </div>
+      <UserInfo />
     </div>
   )
 }
---
+++ web/src/components/common/userInfo.tsx
@@ -0,0 +1,35 @@
+import { useSDK } from '@metamask/sdk-react'
+import { useState } from 'react'
+
+export default function UserInfo() {
+  const [account, setAccount] = useState<string>()
+  const { sdk, connected, connecting, provider, chainId } = useSDK()
+
+  const connect = async () => {
+    try {
+      const accounts: any = await sdk?.connect()
+      setAccount(accounts?.[0])
+    } catch (err) {
+      console.warn(`failed to connect..`, err)
+    }
+  }
+
+  return (
+    <div>
+      {!connected && !connecting && (
+        <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+          Connect
+        </button>
+      )}
+      {connected && (
+        <div>
+          <>
+            {chainId && `Connected chain: ${chainId}`}
+            <p></p>
+            {account && `Connected account: ${account}`}
+          </>
+        </div>
+      )}
+    </div>
+  )
+}
