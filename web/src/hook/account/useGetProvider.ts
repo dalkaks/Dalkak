0b94e0ad3e8cdbe521c13c824ed5fd3374f67424
Louis-Gil <lotusnate@gmail.com>
2024-01-22 21:42:39 +0900

    feat : Update getProvider return when mobile

--- web/src/hook/account/useGetProvider.ts
+++ web/src/hook/account/useGetProvider.ts
@@ -5,7 +5,20 @@ import detectEthereumProvider from '@metamask/detect-provider'
 export const useGetProvider = () => {
   const setAccount = useSetRecoilState(accountState)
 
+  const isMobile = () => {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent,
+    )
+  }
+  const isUnsupportedBrowser = () => {
+    return window.navigator.userAgent.includes('SamsungBrowser')
+  }
+
   const getProvider = async () => {
+    if (isMobile() || isUnsupportedBrowser()) {
+      return
+    }
+
     const provider = (await detectEthereumProvider({
       mustBeMetaMask: true,
       silent: true,
