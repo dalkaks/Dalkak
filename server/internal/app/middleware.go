0a70eb5864002f969ac0d3b06017a711d5b0a6eb
Louis-Gil <lotusnate@gmail.com>
2024-01-25 20:24:44 +0900

    feat : Add verify metamask signature middle ware

--- server/internal/app/middleware.go
+++ server/internal/app/middleware.go
@@ -16,3 +16,16 @@ func (app *APP) enableCORS(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *APP) verifyMetaMaskSignature(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 사인 데이터 추출 및 검증 로직
+
+		// 사인 데이터가 유효하다면, 다음 핸들러로 요청을 전달
+		if true /* isValidSignature(signature) */ {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Invalid MetaMask signature", http.StatusUnauthorized)
+		}
+	})
+}
--- server/pkg/interfaces/application.go
+++ server/pkg/interfaces/application.go
@@ -8,4 +8,5 @@ type Application interface {
 	StartServer(port int, userService UserService) error
 	NewRouter(userService UserService) http.Handler
 	enableCORS(next http.Handler) http.Handler
+	verifyMetaMaskSignature(next http.Handler) http.Handler
 }
