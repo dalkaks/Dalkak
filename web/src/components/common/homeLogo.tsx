a7146580b5080c3d4dfe2045aa76ceeb43db973d
Louis-Gil <lotusnate@gmail.com>
2024-01-31 00:43:12 +0900

    feat : Update web home logo use link

--- web/src/components/common/homeLogo.tsx
+++ web/src/components/common/homeLogo.tsx
@@ -1,13 +1,9 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function HomeLogo() {
-  const handleLogoClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault() // 기본 링크 동작 방지
-    window.location.href = '/' // 홈 화면으로 이동 및 새로고침
-  }
-
   return (
-    <a href="/" onClick={handleLogoClick} className="flex items-center">
+    <Link href="/" passHref className="flex items-center">
       <Image
         src="/favicon.ico"
         alt="Image"
@@ -16,6 +12,6 @@ export default function HomeLogo() {
         className="rounded-md object-cover"
       />
       <h1 className="ml-2 text-2xl font-extrabold tracking-tight">dalkak</h1>
-    </a>
+    </Link>
   )
 }
