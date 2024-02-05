a670ff157297b3b691124e0e7529f285aa2df90f
Louis-Gil <lotusnate@gmail.com>
2024-02-05 23:50:00 +0900

    fix : Update board handler no return

--- server/domain/board/board_handler.go
+++ server/domain/board/board_handler.go
@@ -32,16 +32,19 @@ func (handler *BoardHandler) uploadImage(w http.ResponseWriter, r *http.Request)
 	userInfo, err := httputils.GetUserInfoData(r)
 	if err != nil {
 		httputils.ErrorJSON(w, err, http.StatusUnauthorized)
+    return
 	}
 
   media, err := httputils.GetUploadImageRequest(r)
   if err != nil {
     httputils.ErrorJSON(w, err, http.StatusBadRequest)
+    return
   }
 
   result, err := handler.boardService.UploadImage(media, userInfo)
   if err != nil {
     httputils.ErrorJSON(w, err, http.StatusInternalServerError)
+    return
   }
 
 	httputils.WriteJSON(w, http.StatusOK, result)
