1d322be87638eee8c32755f5484d8536fe338d12
Louis-Gil <lotusnate@gmail.com>
2024-02-07 00:09:29 +0900

    fix : Update allowed image extension

--- server/config/constants.go
+++ server/config/constants.go
@@ -3,10 +3,10 @@ package config
 const MaxUploadSize = 32 << 20 // 32 MB
 
 var AllowedImageExtensions = map[string]bool{
-	".jpg":  true,
-	".jpeg": true,
-	".png":  true,
-	".gif":  true,
-	".bmp":  true,
-	".webp": true,
+	"jpg":  true,
+	"jpeg": true,
+	"png":  true,
+	"gif":  true,
+	"bmp":  true,
+	"webp": true,
 }
