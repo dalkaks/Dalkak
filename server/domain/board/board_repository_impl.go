915a51de44a1122640a3226dbf20f08c89bb91dd
Louis-Gil <lotusnate@gmail.com>
2024-02-11 02:37:31 +0900

    style : Remove no use board presigned api

--- server/domain/board/board_repository_impl.go
+++ server/domain/board/board_repository_impl.go
@@ -1,13 +1,8 @@
 package board
 
 import (
-	"context"
-	"dalkak/pkg/dtos"
 	"dalkak/pkg/interfaces"
-	"dalkak/pkg/utils/timeutils"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
@@ -26,30 +21,30 @@ func NewBoardRepository(db interfaces.Database) *BoardRepositoryImpl {
 	}
 }
 
-func (repo *BoardRepositoryImpl) CreateBoardImage(dto *dtos.BoardImageDto, boardId string) error {
-	newBoardImage := &BoardImageData{
-		Pk:          GenerateBoardDataPk(boardId),
-		Sk:          BoardImageDataType + `#` + dto.Id,
-		EntityType:  BoardImageDataType,
-		Timestamp:   timeutils.GetTimestamp(),
-		Id:          dto.Id,
-		Extension:   dto.Extension,
-		ContentType: dto.ContentType,
-		Url:         dto.Url,
-	}
+// func (repo *BoardRepositoryImpl) CreateBoardImage(dto *dtos.BoardImageDto, boardId string) error {
+// 	newBoardImage := &BoardImageData{
+// 		Pk:          GenerateBoardDataPk(boardId),
+// 		Sk:          BoardImageDataType + `#` + dto.Id,
+// 		EntityType:  BoardImageDataType,
+// 		Timestamp:   timeutils.GetTimestamp(),
+// 		Id:          dto.Id,
+// 		Extension:   dto.Extension,
+// 		ContentType: dto.ContentType,
+// 		Url:         dto.Url,
+// 	}
 
-	av, err := attributevalue.MarshalMap(newBoardImage)
-	if err != nil {
-		return err
-	}
+// 	av, err := attributevalue.MarshalMap(newBoardImage)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	_, err = repo.client.PutItem(context.Background(), &dynamodb.PutItemInput{
-		TableName: aws.String(repo.table),
-		Item:      av,
-	})
-	if err != nil {
-		return err
-	}
+// 	_, err = repo.client.PutItem(context.Background(), &dynamodb.PutItemInput{
+// 		TableName: aws.String(repo.table),
+// 		Item:      av,
+// 	})
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
