ca0197f22d93b93724a2a9535e72defac724075b
junman95 <junman95@naver.com>
2024-02-12 02:08:55 +0900

    chore: setup nextjs internationalization

---
+++ web/app/[lang]/dictionaries.ts
@@ -0,0 +1,12 @@
+import "server-only";
+
+const dictionaries = {
+  en: () =>
+    import("../../dictionaries/en.json").then((module) => module.default),
+  ko: () =>
+    import("../../dictionaries/ko.json").then((module) => module.default),
+};
+
+export type Locale = keyof typeof dictionaries;
+
+export const getDictionary = async (locale: Locale) => dictionaries[locale]();
---
+++ web/middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+let locales = ["en", "ko"];
+
+function getLocale(request: NextRequest) {
+  // Check if the locale is supported
+  const locale =
+    request.cookies.get("locale") ||
+    request.headers.get("Accept-Language") ||
+    "en";
+  return typeof locale === "string" && locales.includes(locale) ? locale : "en";
+}
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  // e.g. incoming request is /products
+  // The new URL is now /en-US/products
+  return Response.redirect(request.nextUrl);
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
