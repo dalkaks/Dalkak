78ba157cf5ac90a99cf2e2e6a4dbdf155c566a85
junman95 <junman95@naver.com>
2024-02-14 00:45:37 +0900

    feat: fetch 모킹 -> 실제 페칭으로 변경을 위한 node-fetch 설치 및 테스트 코드 수정

--- web/app/network/user/post/login.test.ts
+++ web/app/network/user/post/login.test.ts
@@ -1,38 +1,30 @@
-import ENV from "@/app/resources/env-constants";
-import loginService from "./login";
+import ENV from '@/app/resources/env-constants';
+import loginService from './login';
+import mockWallet from '@/mock/walletData.json';
 
-const mockReq = {
-  walletAddress: "myWalletAddress",
-  signature: "mySignature",
-};
-
-const mockRes = {
-  data: {
-    accessToken: "successGetAccessToken",
-  },
-};
-
-jest.spyOn(global, "fetch");
-describe("POST /user/login", () => {
+jest.spyOn(global, 'fetch');
+describe('POST /user/login', () => {
   beforeEach(() => {
     // process.env.SERVER_PATH = "http://test.domain.com";
-    ENV.SERVER_PATH = "http://test.domain.com";
+    ENV.SERVER_PATH = 'http://localhost:80';
   });
-  it("should send a POST request to the correct endpoint", async () => {
-    await loginService(mockReq);
+  it('POST 메소드 작동 횟수 및 데이터 점검', async () => {
+    await loginService(mockWallet);
 
     expect(fetch).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith(`http://test.domain.com/user/login`, {
-      method: "POST",
-      body: JSON.stringify(mockReq),
+    expect(fetch).toHaveBeenCalledWith(`http://localhost:80/user/auth`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(mockWallet)
     });
   });
 
-  it("should return the response", async () => {
-    (fetch as jest.Mock).mockResolvedValueOnce(mockRes);
+  it('로그인 정상작동 - accessToken 존재 유무 및 타입 체크', async () => {
+    const res = await loginService(mockWallet);
 
-    const res = await loginService(mockReq);
-
-    expect(res).toEqual(mockRes);
+    expect(res).toHaveProperty(['data', 'accessToken']);
+    expect(typeof res.data.accessToken === 'string').toBe(true);
   });
 });
