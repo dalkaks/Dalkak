cedf5dfa31dbfbc4d55d439e319159912890a7b4
Louis-Gil <lotusnate@gmail.com>
2024-02-17 01:12:39 +0900

    refactor : Update error flag

--- server/pkg/dtos/error_dto.go
+++ server/pkg/dtos/error_dto.go
@@ -18,30 +18,27 @@ const (
 	ErrCodeNotFound     = http.StatusNotFound
 	ErrCodeInternal     = http.StatusInternalServerError
 
-	ErrMsgJsonInvalid = "json: Invalid JSON"
+	ErrMsgJsonInvalid    = "REQUEST:INVALID_JSON"
+	ErrMsgRequestInvalid = "REQUEST:INVALID_REQUEST"
 
-	ErrMsgTokenParseFailed      = "token: Failed to parse token"
-	ErrMsgTokenExpired          = "token: Token is expired"
-	ErrMsgTokenInvalidClaim     = "token: Invalid token claim"
-	ErrMsgTokenInvalidSignature = "token: Invalid token signature"
-	ErrMsgTokenAccessNotFound   = "token: Access token not found"
-	ErrMsgTokenRefeshNotFound   = "token: Refresh token not found"
-	ErrMsgTokenSignFailed       = "token: Failed to sign token"
+	ErrMsgTokenParseFailed      = "TOKEN:FAILED_TO_PARSE_TOKEN"
+	ErrMsgTokenExpired          = "TOKEN:EPIRED_TOKEN"
+	ErrMsgTokenInvalidClaim     = "TOKEN:INVALID_CLAIM"
+	ErrMsgTokenInvalidSignature = "TOKEN:INVALID_SIGNATURE"
+	ErrMsgTokenAccessNotFound   = "TOKEN:NO_ACCESS_TOKEN"
+	ErrMsgTokenRefeshNotFound   = "TOKEN:NO_REFRESH_TOKEN"
+	ErrMsgTokenSignFailed       = "TOKEN:FAILED_TO_SIGN_TOKEN"
 
-	ErrMsgMediaInvalidType = "media: Invalid media type"
+	ErrMsgMediaInvalidType = "MEDIA:INVALID_TYPE"
+	ErrMsgMediaInvalidKey  = "MEDIA:INVALID_KEY"
+	ErrMsgMediaNotFound    = "MEDIA:NOT_FOUND"
 
-	ErrMsgMediaInvalidKey = "media: Invalid key"
-	ErrMsgMediaNotFound   = "media: Media not found"
+	ErrMsgStorageInvalidURL = "STORAGE:INVALID_URL"
+	ErrMsgStorageNoSuchKey  = "STORAGE:NO_SUCH_KEY"
+	ErrMsgStorageInternal   = "STORAGE:INTERNAL_ERROR"
 
-	ErrMsgDBInternal = "db: Internal error"
-
-	ErrMsgStorageInvalidURL = "storage: Invalid url"
-	ErrMsgStorageNoSuchKey  = "storage: No such key"
-	ErrMsgStorageInternal   = "storage: Internal error"
-
-	ErrMsgRequestInvalid = "request: Invalid request"
-
-	ErrMsgServerInternal = "server: Internal error"
+	ErrMsgDBInternal     = "DB:INTERNAL_ERROR"
+	ErrMsgServerInternal = "SERVER:INTERNAL_ERROR"
 )
 
 func NewAppError(code int, message string, cause ...error) *AppError {
