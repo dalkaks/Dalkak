1d1be87f5ace52787eb7b8ef18ea9a99fb66f2ae
junman <junman9595@gmail.com>
2024-02-20 17:06:58 +0900

    feat: 민팅 폼 스토리 추가

---
+++ web/stories/components/MintingForm.stories.ts
@@ -0,0 +1,23 @@
+import MintingForm from '@/app/[lang]/mint/components/MintingForm';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Component/MintingForm',
+  component: MintingForm,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+  },
+} satisfies Meta<typeof MintingForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'MintingForm',
+  },
+};
