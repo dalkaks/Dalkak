99575cf4e0e4468264e17cdee50763c61092483e
junman95 <junman95@naver.com>
2024-03-07 01:02:35 +0900

    feat: 파일 확장자 수정(영상,이미지만)

--- web/type/nft/fileExtension.ts
+++ web/type/nft/fileExtension.ts
@@ -1,3 +1,3 @@
-type NftFileExt = 'jpg' | 'jpeg' | 'png' | 'gif' | 'mp4' | 'svg' | 'webm' | 'mp3' | 'wav' | 'ogg' | 'glb' | 'gltf'
+type NftFileExt = 'jpg' | 'jpeg' | 'png' | 'gif' | 'mp4' | 'svg' | 'webp';
 
 export type { NftFileExt };
